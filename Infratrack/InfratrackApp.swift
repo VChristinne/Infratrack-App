7204055408de85c80a4ae420c3e2a521167bd5ce
Christinne <vanessa0christinne@gmail.com>
2025-02-11 21:10:35 +0100

    Initial Commit

---
+++ Infratrack/InfratrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfratrackApp.swift
+//  Infratrack
+//
+//  Created by Christinne on 11/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct InfratrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
